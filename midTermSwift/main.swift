bd7d47abbe966fb8bb520d1c2ace50adad1a1289
thodsaphon Sonthiphin <thodsaphonsp@hotmail.co.th>
2018-01-01 17:07:00 +0700

    Initial Commit

---
+++ midTermSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  midTermSwift
+//
+//  Created by thodsaphon on 1/1/2561 BE.
+//  Copyright © 2561 thodsaphon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
